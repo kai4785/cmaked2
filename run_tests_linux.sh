7aa000cde972a5eb9e9fead1d3dadfe532884614
kai <kai@stc-kai-lappy.(none)>
2013-09-10 09:57:59 -0600

    Fixed cmake 2.8.11 support and el6 style cmake28
    Added some logic to detect the version of cmake that changed the way the Determine Compiler works.
    Also added some logic to name the project "cmaked28" if the CMAKE_COMMAND ends with cmake28 to support the cmake28 package on el6 from epel

--- run_tests_linux.sh
+++ run_tests_linux.sh
@@ -5,23 +5,31 @@
 # This script creates a build directory as a peer of cmaked2,
 # and builds the tests there.
 #
-cd ../..
+set -e
+if [ -z "$CMAKE" ]; then
+    cmake=$(which cmake)
+else
+    cmake=$CMAKE
+fi
+test_dir=$(pwd)/tests
+cmaked_dir=$test_dir/cmaked
 rm -rf cmaked_test_build
 mkdir -p cmaked_test_build
-cd cmaked_test_build
+pushd cmaked_test_build
 mkdir -p release
-cd release
+pushd release
 # Do a release build
-cmake ../../cmaked2/tests
+$cmake $test_dir
 make
 make test
-cd ..
+popd
 
 # Do a debug build
 mkdir -p debug
-cd debug
-cmake -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=debug ../../cmaked2/tests
+pushd debug
+$cmake -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=debug $test_dir
 make
 make test
-cd ../..
+popd
+popd
 
